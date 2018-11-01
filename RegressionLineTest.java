73b38d63d5bc73000873a4646b6731314a7db216
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-11-01 10:30:17 -0700

    Update RegressionLineTest.java

--- RegressionLineTest.java
+++ RegressionLineTest.java
@@ -1,3 +1,4 @@
+import javax.swing.JFrame;
 
 public class RegressionLineTest 
 {
@@ -5,8 +6,14 @@ public class RegressionLineTest
 	public static void main(String[] args) 
 	{
 		
+		JFrame frame =  new JFrame();
+		frame.setSize(500, 500);
+		frame.setTitle("Regression Line Application");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
+		RegressionLineComponent component = new RegressionLineComponent( );
+		frame.add(component);
+		frame.setVisible(true);
 	}
 	
 }
