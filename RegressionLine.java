d5aa336ef06b4e006cf9d530390e91a2fc1d9c34
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-11-05 10:30:15 -0700

    Update RegressionLine.java

--- RegressionLine.java
+++ RegressionLine.java
@@ -4,33 +4,51 @@ import java.awt.geom.Line2D;
 public class RegressionLine 
 {
 	
-	public RegressionLine(double x,double y, int clicks)
+	/*
+	public RegressionLine(double x,double y)
 	{
 		
 		x = sum_x;
 		
 		y = sum_y;
 		
-		n = clicks;
+		n = 0;
 		
 		sum_x2 = Math.pow(x, 2);
 		
 		sum_xy = x * y;
 		
 	}
+	*/
 	
 	public void draw(Graphics2D g2, double x2)
 	{
 		
+		Line2D.Double line;
+		
+		double slope;
+		
+		if(n==1)
+		{
+			slope = 0;
+			line = new Line2D.Double();
+		}
+		
 		//compute slope
+		
+		slope = (sum_xy) - (n * (sum_x/n) * (sum_y/n))/
+				sum_x2 - ((sum_x2) - n *(sum_x/n) * (sum_y/n));
+		
+		/*
 		//numerator
 		double top = (sum_xy) - (n * (sum_x/n) * (sum_y/n));
 		//denominator
-		double bot = sum_x2 - (n*Math.pow(sum_x/n, 2));
+		double bot = sum_x2 - ((sum_x2) - n *(sum_x/n) * (sum_y/n));
 		//slope
 		double slope = top/bot;
+		*/
 		
-		Line2D.Double line = new Line2D.Double(0, sum_y/n + slope*(0-sum_x/n), x2-1, sum_y/n + slope*(x2-sum_x/n));
+		line = new Line2D.Double(0, sum_y/n + slope*(0-sum_x/n), x2-1, sum_y/n + slope*(x2-sum_x/n));
 		g2.draw(line);
 		
 	}
@@ -38,7 +56,11 @@ public class RegressionLine
 	public void addPoint(double x, double y)
 	{
 		
-		
+		n++;
+		sum_x=(sum_x+x)/5;
+		sum_y=(sum_y+y)/5;
+		sum_x2=sum_x*sum_x;
+		sum_xy=sum_x*sum_y;
 		
 	}
 	
